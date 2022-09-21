366d70e695171e33e76fb25d216600e7d1a39a42
James <james187a@gmail.com>
2022-09-21 20:13:00 +0200

    Initial commit

---
+++ ChatServer/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ChatServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIP = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnListen = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(12, 15);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(61, 13);
+            this.lblIP.TabIndex = 0;
+            this.lblIP.Text = "IP Address:";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(76, 12);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(162, 20);
+            this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "192.168.1.17";
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(244, 10);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(107, 23);
+            this.btnListen.TabIndex = 2;
+            this.btnListen.Text = "Start Listening";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 38);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(339, 211);
+            this.txtLog.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 261);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnListen);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.lblIP);
+            this.Name = "Form1";
+            this.Text = "Chat Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ ChatServer/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+using static ChatServer.StatusChangedEventArgs;
+
+namespace ChatServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private delegate void UpdateStatusCallback(string strMessage);
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            // Parse the server's IP address out of the TextBox
+            IPAddress ipAddr = IPAddress.Parse(txtIP.Text);
+            // Create a new insantce of the ChatServer object
+            ChatServer mainServer = new ChatServer(ipAddr);
+            // Hook the StatusChanged event handler to mainServer_StatusChanged
+            ChatServer.StatusChanged += new StatusChangedEventHandler(mainServer_StatusChanged);
+            // Start listening for connections
+            mainServer.StartListening();
+            // Show that we started to listen for connections
+            txtLog.AppendText("Monitoring for connections...\r\n");
+        }
+
+        public void mainServer_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            // Call the method that updates the form
+            this.Invoke(new UpdateStatusCallback(this.UpdateStatus), new object[] { e.EventMessage });
+        }
+
+        private void UpdateStatus(string strMessage)
+        {
+            // Updates the log with the message
+            txtLog.AppendText(strMessage + "\r\n");
+        }
+    }
+}
